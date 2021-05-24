0e94284163e1a02ea2798613a1a08af97ddb8ed4
Jesse Bourret - Gheysen <jesse@pop-os.localdomain>
2021-05-24 16:12:01 -0400

    First Commit

---
+++ CellTileMap.gd
@@ -0,0 +1,82 @@
+extends TileMap
+
+
+const TILE_SIZE = 16
+
+export(int) var width
+export(int) var height
+export(int) var speed = 5
+
+var playing = false
+var time = 0
+onready var cam: Camera2D = $ZoomingCamera2D
+
+
+var temp_field 
+
+func _ready():
+	var width_px = width * TILE_SIZE
+	var height_px = height * TILE_SIZE
+	
+	cam.position = Vector2(width_px, height_px)/2
+	cam.zoom = Vector2(width_px, height_px) / Vector2(1024, 768)
+
+	temp_field = []
+	for x in range(width):
+		var temp = []
+		for y in range(height):
+			set_cell(x,y,0)
+			temp.append(0)
+		temp_field.append(temp)
+
+
+func _input(event):
+	if event.is_action_pressed("toggle_play"):
+		playing = !playing
+	if event.is_action_pressed("click"):
+		var pos = (get_local_mouse_position()/TILE_SIZE).floor()
+		set_cellv(pos, 1-get_cellv(pos))
+	if event.is_action_pressed("Zoom_Out"):
+		print("zoom in")
+		cam.zoom_in(Vector2(0,1))
+	if event.is_action_pressed("Zoom_In"):
+		print("zoom out")
+		cam.zoom_out(Vector2(0,0))
+
+
+func _process(delta):
+	time += speed * delta
+	if time > 5:
+		update_field()
+		time = 0
+
+
+func update_field():
+	if !playing:
+		return
+	
+	#adjust state in temp_field
+	for x in range(width):
+		for y in range(height):
+			var live_neighbors = 0
+			for x_off in [-2, -1, 0, 1, 2]:
+				for y_off in [-2, -1, 0, 1, 2]:
+					if (x_off != y_off) or x_off !=0:
+						if get_cell(x+x_off, y+y_off) == 1:
+							live_neighbors += 1
+							
+			if get_cell(x, y) == 1:
+				if live_neighbors in [4,5,6]:
+					temp_field[x][y] = 1
+				else:
+					temp_field[x][y] = 0
+			else:
+				if live_neighbors == 5 or live_neighbors == 6:
+					temp_field[x][y] = 1
+				else:
+					temp_field[x][y] = 0
+	
+	#update the tile map
+	for x in range(width):
+		for y in range(height):
+			set_cell(x, y, temp_field[x][y])
---
+++ ZoomingCamera2D.gd
@@ -0,0 +1,32 @@
+class_name ZoomingCamera2D
+extends Camera2D
+
+# Controls how much we increase or decrease the `_zoom_level` on every turn of the scroll wheel.
+export var zoom_factor := 0.75
+# Duration of the zoom's tween animation.
+export var zoom_duration := 0.25
+
+var current_zoom
+# The camera's target zoom level.
+var _zoom_level := 1.0 
+
+# We store a reference to the scene's tween node.
+onready var tween: Tween = $Tween
+onready var max_zoom = zoom.x * 3
+onready var min_zoom = (max_zoom/3) * zoom_factor
+
+
+func zoom_in(new_offset):
+	transition_camera(Vector2(min_zoom, min_zoom), new_offset)
+
+
+func zoom_out(new_offset):
+	transition_camera(Vector2(max_zoom, max_zoom), new_offset)
+
+
+func transition_camera(new_zoom, new_offset):
+	if new_zoom != current_zoom:
+		current_zoom = new_zoom
+		$Tween.interpolate_property(self, "zoom", get_zoom(), current_zoom, zoom_duration, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+		$Tween.interpolate_property(self, "offset", get_offset(), new_offset, zoom_duration, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+		$Tween.start()
