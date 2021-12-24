700661455c2c6744de12e31b633763ed8a867d7c
jbourret <jbourret@uwo.ca>
2021-12-24 10:29:19 -0500

    Testing

--- Player.gd
+++ Player.gd
@@ -17,6 +17,8 @@ func _input(event):
 	if event.is_action_pressed("hide"):
 		# Show/Hide the player
 		pass
+	if event.is_action_pressed("clear"):
+		pass
 	if event.is_action_pressed("ui_select"):
 		# Change tile.. same as click
 		var pos = ((self.position + Vector2(8, 8))/tile_map.TILE_SIZE).floor()
